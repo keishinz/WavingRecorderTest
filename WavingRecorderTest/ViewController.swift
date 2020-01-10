3ef18b8df7b96ba2e7f10c25c43ac26e6857e16e
Keishin CHOU <keishin.chou@gmail.com>
2020-01-10 17:07:37 +0900

    Initial Commit

---
+++ WavingRecorderTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WavingRecorderTest
+//
+//  Created by Keishin CHOU on 2020/01/10.
+//  Copyright © 2020 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
